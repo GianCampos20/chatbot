65011fe2af5b066af1f2f5ec5c2752ba327718b2
GianCampos20 <gianfrancocamposfp@gmail.com>
2025-04-22 11:34:22 +0200

    Clase que contiene los metodos de las Urls de la API y su clave

---
+++ src/Router.py
@@ -0,0 +1,8 @@
+class Router:
+    
+    def returnApiKey():
+        
+        return "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"
+    
+    def baseUrl():
+        return "https://openrouter.ai/api/v1"
