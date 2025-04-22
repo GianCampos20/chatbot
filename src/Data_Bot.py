3ba79072bca0b1ff787d6d96c4ec3e88b5f7aaab
GianCampos20 <gianfrancocamposfp@gmail.com>
2025-04-22 11:33:29 +0200

    Clase que obtiene los datos del Bot

---
+++ src/Data_Bot.py
@@ -0,0 +1,11 @@
+class Data_Bot:
+    
+    def get_data(message):
+        
+        return {
+        "model" : "openai/gpt-3.5-turbo",
+        "messages" : [
+            {"role" : "system", "content" : "Eres un asistente útil y simpático"},
+            {"role" : "user", "content" : message}  
+        ]
+     }
