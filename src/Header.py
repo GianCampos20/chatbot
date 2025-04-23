ccf84f36ea07d529a382d14b40125893a857df6a
GianCampos20 <gianfrancocamposfp@gmail.com>
2025-04-23 21:55:27 +0200

    nueva variable para obtener el referer

--- src/Header.py
+++ src/Header.py
@@ -1,12 +1,14 @@
 from src.Router import Router
-
+import os
 class Header:
     def get_headers():
         OPENROUTER_API_KEY = Router.returnApiKey()
+        
+        referer = os.environ.get("REFERER", "http://localhost:5000")
     
         return {
         "Authorization": f"Bearer {OPENROUTER_API_KEY}",
         "Content-Type": "application/json",
-        "Referer" : "https://chatbot-l99s.onrender.com",
+        "Referer" : referer,
         "X-Title" : "Mi Chatbot Flask"
         }
