329f64b366666f88fab3d917104e93bde813ce83
GianCampos20 <gianfrancocamposfp@gmail.com>
2025-04-23 21:59:52 +0200

    nueva forma de obtener la clave de la API

--- src/Header.py
+++ src/Header.py
@@ -2,12 +2,12 @@ from src.Router import Router
 import os
 class Header:
     def get_headers():
-        OPENROUTER_API_KEY = Router.returnApiKey()
+        # OPENROUTER_API_KEY = Router.returnApiKey()
         
         referer = os.environ.get("REFERER", "http://localhost:5000")
     
         return {
-        "Authorization": f"Bearer {OPENROUTER_API_KEY}",
+        "Authorization": "Bearer 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",
         "Content-Type": "application/json",
         "Referer" : referer,
         "X-Title" : "Mi Chatbot Flask"
