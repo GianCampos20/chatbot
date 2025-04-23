2a83728e32feffafde908bc635ddcbc7346aed3b
GianCampos20 <gianfrancocamposfp@gmail.com>
2025-04-23 22:00:20 +0200

    nuevo cambio para obtener la clave

--- src/Router.py
+++ src/Router.py
@@ -1,10 +1,8 @@
-import os
-
 class Router:
     
-    @staticmethod
     def returnApiKey():
-        return os.getenv("OPENROUTER_API_KEY")
+        
+        return "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"
     
     def baseUrl():
         return "https://openrouter.ai/api/v1"
