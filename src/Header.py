88ba4d3b2b1215588b77471fe6016aa3ce60e8b3
GianCampos20 <gianfrancocamposfp@gmail.com>
2025-04-24 19:38:44 +0200

    Eliminacion de print en la clase Header

--- src/Header.py
+++ src/Header.py
@@ -3,10 +3,9 @@ class Header:
     def get_headers():
         from src.Router import Router
         api_key = Router.returnApiKey()
-        print("🧪 Usando API Key:", api_key[:10] + "...")  # para debug
         return {
             "Authorization": f"Bearer {api_key}",
             "Content-Type": "application/json",
-            "Referer": "https://chatbot-l99s.onrender.com",  # Cambia por tu dominio real
+            "Referer": "https://chatbot-l99s.onrender.com",
             "X-Title": "Mi Chatbot Flask"
         }
